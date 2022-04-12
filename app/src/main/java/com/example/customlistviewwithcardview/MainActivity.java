11db580b6a88d38d5e8adf3d1b88189009260e84
Baraka Danie <barakadaniel116@gmail.com>
2022-04-12 18:55:37 +0300

    commit

---
+++ app/src/main/java/com/example/customlistviewwithcardview/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.customlistviewwithcardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+ListView listView;
+List<Pojo> list;
+MyAdapter myAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.list_view);
+        listShow();
+        myAdapter=new MyAdapter(this,list);
+        listView.setAdapter(myAdapter);
+
+    }
+    private void listShow()
+    {
+        list=new ArrayList<>();
+        list.add(new Pojo(R.drawable.ic_wifi,"Wifi","This is a wifi icon"));
+        list.add(new Pojo(R.drawable.ic_access_alarm,"Alarm","This is an alarm Icon"));
+        list.add(new Pojo(R.drawable.ic_battery_full,"Battery Full","This is battery full icon"));
+        list.add(new Pojo(R.drawable.ic_bluetooth,"Bluetooth","This is a bluetooth icon"));
+        list.add(new Pojo(R.drawable.ic_rotation,"Screen rotation","this is a screen rotation icon"));
+        list.add(new Pojo(R.drawable.ic_wallpaper,"Wallpaper","This is a wallpaper icon"));
+        list.add(new Pojo(R.drawable.ic_storage,"Storage","This is a storage icon"));
+        list.add(new Pojo(R.drawable.ic_wifi,"Wifi","This is a wifi icon"));
+        list.add(new Pojo(R.drawable.ic_access_alarm,"Alarm","This is an alarm Icon"));
+        list.add(new Pojo(R.drawable.ic_battery_full,"Battery Full","This is battery full icon"));
+        list.add(new Pojo(R.drawable.ic_bluetooth,"Bluetooth","This is a bluetooth icon"));
+        list.add(new Pojo(R.drawable.ic_rotation,"Screen rotation","this is a screen rotation icon"));
+        list.add(new Pojo(R.drawable.ic_wallpaper,"Wallpaper","This is a wallpaper icon"));
+        list.add(new Pojo(R.drawable.ic_storage,"Storage","This is a storage icon"));
+    }
+}
